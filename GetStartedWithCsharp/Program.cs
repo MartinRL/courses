94dd4cb67f71ec0f7d136f71ce7533db0eeaecfa
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-07-11 16:06:10 +0200

    test :green_heart:

--- GetStartedWithCsharp/Program.cs
+++ GetStartedWithCsharp/Program.cs
@@ -6,7 +6,15 @@ namespace GetStartedWithCsharp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.");
+            string projectName = "ACME";
+
+            string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+            Console.WriteLine(@$"View English output:
+  c:\Exercise\{projectName}\data.txt
+
+{russianMessage}:
+  c:\Exercise\{projectName}\ru-RU\data.txt");
         }
     }
 }
