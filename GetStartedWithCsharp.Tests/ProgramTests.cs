cad5dd770d7f35e2092f297f9ed90d201f04efaa
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-07-11 15:20:15 +0200

    boilerplate w/ approval test

---
+++ GetStartedWithCsharp.Tests/ProgramTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace GetStartedWithCsharp.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public async Task VerifyConsoleOutput()
+        {
+            // Capture the console output
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                // Call the main method of the console application
+                Program.Main(args: null);
+
+                var output = sw.ToString().Trim();
+
+                // Verify the output using Verify.Xunit
+                await Verify(output);
+            }
+        }
+    }
+}
--- GetStartedWithCsharp/Program.cs
+++ GetStartedWithCsharp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace GetStartedWithCsharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("This is the first line.");
+            //Console.WriteLine("This is the second line.");
+        }
+    }
+}
