a3b8b8e62599357839684b6c501f4e00e46346a2
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-07-15 13:30:05 +0200

    :red_circle: test
    https://learn.microsoft.com/en-us/training/modules/csharp-basic-operations/5-challenge

---
+++ GetStartedWithCsharp.Tests/MiscTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+
+namespace GetStartedWithCsharp.Tests
+{
+    public class TempFormatter
+    {
+        private const int fahrenheit = 94;
+
+        public string CelsiusMsg = $"The temperature is {((decimal)(fahrenheit - 32) * 5 / 9):N1} Celsius.";
+    }
+
+    public class Tests
+    {
+        [Fact]
+        public void VerifyCelsiusMsg() => new TempFormatter().CelsiusMsg.Should().Be("The temperature is 34.4 Celsius.");
+    }
+}
