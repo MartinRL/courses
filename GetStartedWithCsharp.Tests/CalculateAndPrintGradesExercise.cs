3cc2df6de0d03072586232810d90036a2936dd56
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-07-15 13:53:14 +0200

    init

---
+++ GetStartedWithCsharp.Tests/CalculateAndPrintGradesExercise.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetStartedWithCsharp.Tests
+{
+    public class CalculateAndPrintGrades
+    {
+        public void Execute()
+        {
+            // initialize variables - graded assignments 
+            int currentAssignments = 5;
+
+            int sophia1 = 93;
+            int sophia2 = 87;
+            int sophia3 = 98;
+            int sophia4 = 95;
+            int sophia5 = 100;
+
+            int nicolas1 = 80;
+            int nicolas2 = 83;
+            int nicolas3 = 82;
+            int nicolas4 = 88;
+            int nicolas5 = 85;
+
+            int zahirah1 = 84;
+            int zahirah2 = 96;
+            int zahirah3 = 73;
+            int zahirah4 = 85;
+            int zahirah5 = 79;
+
+            int jeong1 = 90;
+            int jeong2 = 92;
+            int jeong3 = 98;
+            int jeong4 = 100;
+            int jeong5 = 97;
+
+            int sophiaSum = 0;
+            int nicolasSum = 0;
+            int zahirahSum = 0;
+            int jeongSum = 0;
+
+            Console.WriteLine("Sophia: " + sophiaSum);
+            Console.WriteLine("Nicolas: " + nicolasSum);
+            Console.WriteLine("Zahirah: " + zahirahSum);
+            Console.WriteLine("Jeong: " + jeongSum);
+        }
+    }
+
+    public class CalculateAndPrintGradesTests
+    {
+    }
+}
