6dbbcc1eccdbafb05a107189aa80ff20860d1189
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-08-14 16:06:08 +0200

    tests and boilerplate
    https://learn.microsoft.com/en-us/training/modules/csharp-evaluate-boolean-expressions/6-challenge-2

---
+++ GetStartedWithCsharp/PermissionAndLevelBooleanLogicChallenge.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+
+namespace GetStartedWithCsharp;
+
+internal class PermissionAndLevel
+{
+    internal static string CreateWelcomeMessage(string permission, int level) => throw new NotImplementedException();
+}
+
+public class PermissionAndLevelTests
+{
+    [Theory]
+    [InlineData("Admin", 60, "Welcome, Super Admin user.")]
+    [InlineData("Admin", 55, "Welcome, Admin user.")]
+    [InlineData("Manager", 25, "Contact an Admin for access.")]
+    [InlineData("Manager", 19, "You do not have sufficient privileges.")]
+    [InlineData("User", 30, "You do not have sufficient privileges.")]
+    [InlineData("Admin", 40, "Welcome, Admin user.")]
+    [InlineData("Manager", 20, "Contact an Admin for access.")]
+    [InlineData("Manager", 5, "You do not have sufficient privileges.")]
+    public void VerifyPermissionAndLevelFor(string permission, int level, string expectedMessage)
+    {
+        string actualMessage = PermissionAndLevel.CreateWelcomeMessage(permission, level);
+
+        actualMessage.Should().Be(expectedMessage);
+    }
+}
