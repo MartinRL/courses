100201e17fe08cf991777489d45ac2c6e83314dd
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-08-14 16:28:40 +0200

    updated tests w/ all test cases :green_heart:

--- GetStartedWithCsharp/PermissionAndLevelBooleanLogicChallenge.cs
+++ GetStartedWithCsharp/PermissionAndLevelBooleanLogicChallenge.cs
@@ -4,7 +4,12 @@ namespace GetStartedWithCsharp;
 
 internal class PermissionAndLevel
 {
-    internal static string CreateWelcomeMessage(string permission, int level) => throw new NotImplementedException();
+    internal static string CreateWelcomeMessage(string permission, int level) =>
+        permission.Contains("Admin") && level > 55 ? "Welcome, Super Admin user."
+        : permission.Contains("Admin") && level <= 55 ? "Welcome, Admin user."
+        : permission.Contains("Manager") && level >= 20 ? "Contact an Admin for access."
+        : permission.Contains("Manager") && level < 20 ? "You do not have sufficient privileges."
+        : "You do not have sufficient privileges.";
 }
 
 public class PermissionAndLevelTests
@@ -12,12 +17,13 @@ public class PermissionAndLevelTests
     [Theory]
     [InlineData("Admin", 60, "Welcome, Super Admin user.")]
     [InlineData("Admin", 55, "Welcome, Admin user.")]
-    [InlineData("Manager", 25, "Contact an Admin for access.")]
-    [InlineData("Manager", 19, "You do not have sufficient privileges.")]
-    [InlineData("User", 30, "You do not have sufficient privileges.")]
-    [InlineData("Admin", 40, "Welcome, Admin user.")]
     [InlineData("Manager", 20, "Contact an Admin for access.")]
-    [InlineData("Manager", 5, "You do not have sufficient privileges.")]
+    [InlineData("User|Manager", 55, "Contact an Admin for access.")]
+    [InlineData("Manager", 19, "You do not have sufficient privileges.")]
+    [InlineData("Admin|Manager", 60, "Welcome, Super Admin user.")]
+    [InlineData("Admin|Manager", 55, "Welcome, Admin user.")]
+    [InlineData("User", 10, "You do not have sufficient privileges.")]
+    [InlineData("Guest", 1, "You do not have sufficient privileges.")]
     public void VerifyPermissionAndLevelFor(string permission, int level, string expectedMessage)
     {
         string actualMessage = PermissionAndLevel.CreateWelcomeMessage(permission, level);
