fab61e092707f54e47475169165bb3d857846835
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-08-15 11:30:45 +0200

    add failing FizzBuzz test :red_circle:
    https://learn.microsoft.com/en-us/training/modules/csharp-for/3-challenge

---
+++ GetStartedWithCsharp/FizzBuzzExcercise.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+
+namespace GetStartedWithCsharp;
+
+public class FizzBuzz
+{
+    internal static IEnumerable<string> Generate(int count) => throw new NotImplementedException();
+}
+
+public class FizzBuzzTests
+{
+    [Fact]
+    public void GenerateFizzBuzz_ShouldReturnExpectedSequence()
+    {
+        // Arrange
+        var expected = new List<string>
+        {
+            "1",
+            "2",
+            "3 - Fizz",
+            "4",
+            "5 - Buzz",
+            "6 - Fizz",
+            "7",
+            "8",
+            "9 - Fizz",
+            "10 - Buzz",
+            "11",
+            "12 - Fizz",
+            "13",
+            "14",
+            "15 - FizzBuzz",
+            "16",
+            "17",
+            "18 - Fizz",
+            "19",
+            "20 - Buzz",
+            "21 - Fizz"
+        };
+
+        // Act
+        var result = FizzBuzz.Generate(21);
+
+        // Assert
+        result.Should().BeEquivalentTo(expected);
+    }
+}
